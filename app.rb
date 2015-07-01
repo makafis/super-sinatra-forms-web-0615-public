3c516200d769c922a1b9d53eb3e525ee08435a92
makafis <vinnyalfieri@gmail.com>
2015-07-01 16:45:56 -0400

    complete

--- app.rb
+++ app.rb
@@ -2,4 +2,17 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+  get '/' do
+      
+    erb :'super_hero'  
+  end
+
+  post '/teams' do
+    
+    @team = params[:team] 
+    @members = params[:team][:members]
+    # binding.pry
+    erb :'team'
+  end
+
 end
